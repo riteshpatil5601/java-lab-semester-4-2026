9bffe303523c5ca0d488ad67455c6d65e4214a97
riteshpatil5601 <riteshpatil56019210@gmail.com>
2026-01-13 10:27:13 +0530

    Add files via upload

---
+++ operations.java
@@ -0,0 +1,17 @@
+public class operations 
+{
+    public static void main(String[]a)
+    {  
+        float d=3.4f;
+        float delta=7.8f;
+       
+        for(int i=0;i<5;i++)
+        {
+        System.out.println("ADDITION :" + (d+delta));
+        for(int j=0;j<10;j++)
+        {
+        System.out.println("    "+"SUBSTRACTION :"+(delta - d));
+        }
+        }
+    }
+}
---
+++ opp.java
@@ -0,0 +1,41 @@
+public class opp 
+{
+    
+    public static void main( String []ar)
+    {
+        int a=2;
+        int b =3;
+        int c=4;
+        int d=5;
+
+        int value=0;
+        while(value<7){
+        if(value==1)
+        {
+            System.out.println("a+b:"+(a+b));
+        }
+        else if(value==2)
+        {
+            System.out.println("d-a:"+(d-a));
+        }
+        else if(value==3)
+        {
+            System.out.println("c*d:"+(c*d));
+        }
+        else if(value==4)
+        {
+            System.out.println("d/a:"+(d/a));
+        }
+        else if (value==5)
+        {
+            System.out.println("d%b:"+(d%b));
+        }
+        else
+        {
+            System.out.println(" JAY HIND , JAY MAHARASHTRA....................................");
+        }
+        value ++;
+        }
+
+    }
+}
